caa2a903b9be46c17b7ee0594ca3f45296525372
Lancel <maurotoscano2@gmail.com>
2019-05-29 22:01:48 -0300

    Descartadas entradas con corchetes en el path

--- TP2/desagregacion/test.py
+++ TP2/desagregacion/test.py
@@ -5,7 +5,6 @@ export LD_LIBRARY_PATH=/usr/local/lib
 import pybgpstream
 import pprint
 import numpy as np
-import pickle
 #---------------------------------------------------------
 # Definiciones
 
@@ -25,12 +24,28 @@ stream = pybgpstream.BGPStream(from_time=TIME_INIT,
 #---------------------------------------------------------
 
 # Ejecucion y parseo de la consulta
+#
+
 
 # Este loop puede tardar mucho porque la RIB que estamos mirando es sumamente grande
-full_bgp = list()
+table = list()
+cant_usadas = 0
+cant_descartadas = 0
 for elem in stream:
-    full_bgp.append(elem.fields)
+    as_path = elem.fields["as-path"]
+    #Algunas entradas tienen arrays adentro, no se como interpretarlas, las descarto
+    if not "}" in as_path:
+        cant_usadas = cant_usadas + 1
+        as_path_list = as_path.split(' ')
+        as_id = int(as_path_list[-1])
+        prefix = elem.fields["prefix"]
+        table.append((as_id, prefix))
+    else:
+        cant_descartadas = cant_descartadas + 1
 
-import pickle
-with open("test.txt", "wb") as fp:   #Pickling
-    pickle.dump(full_bgp, fp)
+print(
+    "Se descartaron el ", \
+    float(cant_descartadas)/(cant_descartadas + cant_usadas) * 100, \
+    "% de las entradas")
+#np.save("full_bgp_file",full_bgp) # Graba la lista, cuidado, usa unos 17GiB de ram para hacerlo
+#full_bgp = test_load = np.load("test.npy").tolist() # Carga la lista
